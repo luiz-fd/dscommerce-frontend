b3e62174b49cfabdd9ffdb9b69c517596b79e0ed
Luiz F Dias <luizfd@gmail.com>
2023-09-16 20:59:38 -0300

    ProductDetails Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-
-import './App.css'
-import HeaderClient from './components/HeaderClient'
-import ButtonPrimary from './components/ButtonPrimary'
-import ButtonInverse from './components/ButtonInverse'
-import ProductDetailsCard from './components/ProductDetailsCard'
+import ProductDetails from './routes/ProductDetails'
 
 function App() {
   return (
-    <>
-      <HeaderClient></HeaderClient>
-    <main>
-      <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard></ProductDetailsCard>
-        <div className="dsc-btn-page-container">
-            <ButtonPrimary></ButtonPrimary>
-            <ButtonInverse></ButtonInverse>
-        </div>
-      </section>
-    </main>
-    </>
+    <ProductDetails/>
   )
 }
 
