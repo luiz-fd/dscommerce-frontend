b583b052b6ce33c9b76cb283cc1a6fcd9e8e5fab
Luiz F Dias <luizfd@gmail.com>
2023-09-24 10:53:01 -0300

    Add Props in Catalog Component

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -3,6 +3,25 @@ import HeaderClient from "../../components/HeaderClient";
 import SearchBar from "../../components/SearchBar";
 import CatalogCard from "../../components/CatalogCard";
 import ButtonNextPage from "../../components/ButtonNextPage";
+import { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "GOOD",
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources-devsuperior/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories:[
+    {
+      id:1,
+      name:"Eletrônicos"
+    },
+    {
+      id:2,
+      name:"Computadores"
+    }
+  ]
+}
 
 export default function Catalog() {
   return (
@@ -12,16 +31,16 @@ export default function Catalog() {
         <section id="catalog-section" className="dsc-container">
           <SearchBar></SearchBar>
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
-            <CatalogCard></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
+            <CatalogCard product={product}></CatalogCard>
           </div>
           <ButtonNextPage></ButtonNextPage>          
         </section>
