d2dd1f2b4303ecb9c818e23c768ac0302b60a31c
Luiz F Dias <luizfd@gmail.com>
2023-09-26 19:45:06 -0300

    Adding React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 
-import Catalog from './routes/Catalog'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProductDetails from './routes/ClientHome/ProductDetails'
+import ClientHome from './routes/ClientHome'
+import Catalog from './routes/ClientHome/Catalog'
 
 function App() {
   return (
-    <Catalog/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ClientHome></ClientHome>}>
+        <Route index element={<Catalog></Catalog>}></Route>
+        <Route path='catalog' element={<Catalog></Catalog>}></Route>
+        <Route path='product-details' element={<ProductDetails></ProductDetails>}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -0,0 +1,47 @@
+import "./styles.css";
+import SearchBar from "../../../components/SearchBar";
+import CatalogCard from "../../../components/CatalogCard";
+import ButtonNextPage from "../../../components/ButtonNextPage";
+import { ProductDTO } from "../../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "GOOD",
+  imgUrl:
+    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources-devsuperior/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 1,
+      name: "Eletrônicos",
+    },
+    {
+      id: 2,
+      name: "Computadores",
+    },
+  ],
+};
+
+export default function Catalog() {
+  return (
+    <main>
+      <section id="catalog-section" className="dsc-container">
+        <SearchBar></SearchBar>
+        <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+          <CatalogCard product={product}></CatalogCard>
+        </div>
+        <ButtonNextPage></ButtonNextPage>
+      </section>
+    </main>
+  );
+}
