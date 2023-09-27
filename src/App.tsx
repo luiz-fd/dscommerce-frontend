2ab176ca83269281e0a7efafcf6c5455e537b492
Luiz F Dias <luizfd@gmail.com>
2023-09-26 21:44:04 -0300

    NotFound route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import ProductDetails from './routes/ClientHome/ProductDetails'
-import ClientHome from './routes/ClientHome'
-import Catalog from './routes/ClientHome/Catalog'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ProductDetails from "./routes/ClientHome/ProductDetails";
+import ClientHome from "./routes/ClientHome";
+import Catalog from "./routes/ClientHome/Catalog";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<ClientHome></ClientHome>}>
-        <Route index element={<Catalog></Catalog>}></Route>
-        <Route path='catalog' element={<Catalog></Catalog>}></Route>
-        <Route path='product-details/:productId' element={<ProductDetails></ProductDetails>}></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<ClientHome></ClientHome>}>
+          <Route index element={<Catalog></Catalog>}></Route>
+          <Route path="catalog" element={<Catalog></Catalog>}></Route>
+          <Route
+            path="product-details/:productId"
+            element={<ProductDetails></ProductDetails>}
+          ></Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
