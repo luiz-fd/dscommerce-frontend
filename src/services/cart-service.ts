88fd7a228693ef0ed4a7ea7292b0739b5d24bae9
Luiz F Dias <luizfd@gmail.com>
2023-10-01 21:09:08 -0300

    Cart decreaseItem

--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -6,30 +6,43 @@ export function saveCart(cart: OrderDTO) {
     cartRepository.save(cart);
 }
 
-export function getCart():OrderDTO{
+export function getCart(): OrderDTO {
     return cartRepository.get();
 }
 
-export function addProduct(product:ProductDTO){
+export function addProduct(product: ProductDTO) {
     const cart = cartRepository.get();
     const item = cart.items.find(x => x.productId === product.id);
-    if(!item){
-        const newItem = new OrderItemDTO(product.id,1,product.name,product.price,product.imgUrl);
+    if (!item) {
+        const newItem = new OrderItemDTO(product.id, 1, product.name, product.price, product.imgUrl);
         cart.items.push(newItem);
         cartRepository.save(cart);
     }
 
 }
 
-export function clearCart(){
+export function clearCart() {
     cartRepository.clear();
 }
 
-export function increaseItem(productId:number){
+export function increaseItem(productId: number) {
     const cart = cartRepository.get();
-    const item =  cart.items.find(x => x.productId === productId);
-    if(item){
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
         item.quantity++;
+        cartRepository.save(cart);
+    }
+}
+
+export function decreaseItem(productId: number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        if (item.quantity > 1) {
+            item.quantity--;
+        } else {
+            cart.items = cart.items.filter(x => x.productId !== productId);
+        }
         cartRepository.save(cart);
     }
 }
