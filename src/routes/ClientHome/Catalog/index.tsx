2c452ddea85336434cc8bafb325073d27183df4b
Luiz F Dias <luizfd@gmail.com>
2023-10-02 19:29:45 -0300

    Searchbar onSearch event

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -8,19 +8,25 @@ import * as productService from "../../../services/product-service"
 
 export default function Catalog() {
 
-  const [products, setProducts] = useState<ProductDTO[]>([])
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  const [productName, setProductName] = useState("");
 
   useEffect(() => {
-    productService.findPageRequest(0,"ma",12,"name")
+    productService.findPageRequest(0,productName)
     .then(response => {
       setProducts(response.data.content);
     })
-  },[]);
+  },[productName]);
+
+  function handleSearch(searchText:string){
+    setProductName(searchText)
+  }
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar></SearchBar>
+        <SearchBar onSearch={handleSearch}></SearchBar>
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {products.map((product) => (
             <CatalogCard key={product.id} product={product}></CatalogCard>
