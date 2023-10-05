4483086bd35b2facbb482d90aa6886f64f29cd3d
Luiz F Dias <luizfd@gmail.com>
2023-10-04 22:38:40 -0300

    Interceptors

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -2,15 +2,45 @@ import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
 import * as authService from '../services/auth-service'
 
-export function requestBackend(config:AxiosRequestConfig){
+export function requestBackend(config: AxiosRequestConfig) {
 
     const headers = config.withCredentials
-    ? {
-        ...config.headers,
-        Authorization: "Bearer " + authService.getAccessToken()
-    }
-    : config.headers;
-    
+        ? {
+            ...config.headers,
+            Authorization: "Bearer " + authService.getAccessToken()
+        }
+        : config.headers;
 
-    return axios({...config, baseURL: BASE_URL, headers});
+
+    return axios({ ...config, baseURL: BASE_URL, headers });
 }
+
+// REQUEST INTERCEPTOR
+axios.interceptors.request.use(
+    function (config) {
+        // DO SOMETHING BEFORE REQUEST IS SENT
+        return config;
+    },
+    function (error) {
+        // DO SOMETHING WITH REQUEST ERROR
+        return Promise.reject(error);
+    }
+);
+
+// RESPONSE INTERCEPTOR
+axios.interceptors.response.use(
+    function (response) {
+        // DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+        return response;
+    },
+    function (error) {
+        if(error.response.status === 401){
+            console.log("401")
+        }
+        if(error.response.status === 403){
+            console.log("403")
+        }
+        // DO SOMETHING WITH RESPONSE ERROR
+        return Promise.reject(error);
+    }
+);
