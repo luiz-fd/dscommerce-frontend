dc0d5d54582990e8f4fc745fecb81e1ca1f6dbd4
Luiz F Dias <luizfd@gmail.com>
2023-10-05 22:53:02 -0300

    Confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { AccessTokenPayloadDTO } from "./models/auth";
 import { ContextToken } from "./utils/context-token";
 import * as authService from "./services/auth-service";
 import * as cartService from "./services/cart-service";
+import Confirmation from "./routes/ClientHome/Confirmation";
 
 function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -46,6 +47,10 @@ function App() {
               ></Route>
               <Route path="cart" element={<Cart></Cart>}></Route>
               <Route path="login" element={<Login></Login>}></Route>
+              <Route
+                path="confirmation/:orderId"
+                element={<Confirmation />}
+              ></Route>
             </Route>
             <Route
               path="/admin/"
