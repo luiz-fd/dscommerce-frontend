0f8f1ed4be028ad11a8bc346d1d10fc9a6d7cc2f
Luiz F Dias <luizfd@gmail.com>
2023-10-10 18:44:26 -0300

    New Form Struct

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,75 +1,99 @@
-import { useState,useContext } from 'react';
-import { CredentialsDTO } from '../../../models/auth';
-import './styles.css'
-import * as authService from '../../../services/auth-service'
-import { useNavigate } from 'react-router-dom';
-import { ContextToken } from '../../../utils/context-token';
+import { useState, useContext } from "react";
+import { CredentialsDTO } from "../../../models/auth";
+import "./styles.css";
+import * as authService from "../../../services/auth-service";
+import { useNavigate } from "react-router-dom";
+import { ContextToken } from "../../../utils/context-token";
 
-export default function Login(){
+export default function Login() {
   const { setContextTokenPayload } = useContext(ContextToken);
   const navigate = useNavigate();
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        password:"",
-        username:""
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    function handleSubmit(event:any){
-        event.preventDefault();
-        authService.loginRequest(formData)
-        .then(response => {
-          authService.saveAccessToken(response.data.access_token)
-          setContextTokenPayload(authService.getAccessTokenPayload())
-          navigate("/cart");
-        })
-        .catch(error => {
-            console.log("Erro no Login" + error)
-        })
-    }
+  const [formData, setFormData] = useState<any>({
+    username: {
+      value: "",
+      id: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Email",
+      validation: function (value: string) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase()
+        );
+      },
+      message: "Favor informar um email válido",
+    },
+    password: {
+      value: "",
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Senha",
+    },
+  });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    authService
+      .loginRequest({
+        username: formData.username.value,
+        password: formData.password.value
+      })
+      .then((response) => {
+        authService.saveAccessToken(response.data.access_token);
+        setContextTokenPayload(authService.getAccessTokenPayload());
+        navigate("/cart");
+      })
+      .catch((error) => {
+        console.log("Erro no Login" + error);
+      });
+  }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    function handleInputChange(event:any){
-        const value = event.target.value;
-        const name = event.target.name;
-        setFormData({...formData, [name]: value})
-    }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: {...formData[name], value: value} });
+  }
 
-    return(
-        <main>
-        <section id="login-section" className="dsc-container">
-          <div className="dsc-login-form-container">
-            <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
-              <h2>Login</h2>
-              <div className="dsc-form-controls-container">
-                <div>
-                  <input 
-                  name="username" 
-                  value={formData.username}
-                  className="dsc-form-control" 
-                  type="text" 
-                  placeholder="Email" 
+  return (
+    <main>
+      <section id="login-section" className="dsc-container">
+        <div className="dsc-login-form-container">
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="dsc-form-controls-container">
+              <div>
+                <input
+                  name="username"
+                  value={formData.username.value}
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Email"
                   onChange={handleInputChange}
-                  />
-                  <div className="dsc-form-error"></div>
-                </div>
-                <div>
-                  <input 
-                  name='password'
-                  value={formData.password}
-                  className="dsc-form-control" 
-                  type="password" 
-                  placeholder="Senha" 
+                />
+                <div className="dsc-form-error"></div>
+              </div>
+              <div>
+                <input
+                  name="password"
+                  value={formData.password.value}
+                  className="dsc-form-control"
+                  type="password"
+                  placeholder="Senha"
                   onChange={handleInputChange}
-                  />
-                </div>
+                />
               </div>
-  
-              <div className="dsc-login-form-buttons dsc-mt20">
-                <button type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
-    );
+            </div>
+
+            <div className="dsc-login-form-buttons dsc-mt20">
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Entrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
