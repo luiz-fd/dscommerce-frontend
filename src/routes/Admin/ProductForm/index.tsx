a7bbd0f0c83d526ab41891fff2918905fa97de6f
Luiz F Dias <luizfd@gmail.com>
2023-10-12 09:04:43 -0300

    FormTextArea

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import FormInput from "../../../components/FormInput";
 import * as forms from "../../../utils/forms";
 import * as productService from '../../../services/product-service';
+import FormTextArea from "../../../components/FormTextArea";
 
 export default function ProductForm() {
   const params = useParams();
@@ -39,6 +40,17 @@ export default function ProductForm() {
       name: "imgUrl",
       type: "text",
       placeholder: "Imagem",
+    },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição",
+      validation: function(value:string){
+        return /^.{10,}$/.test(value);
+      },
+      message: "Favor informar uma descrição de no mínimo 10 caracteres"
     },
   });
 
@@ -96,6 +108,15 @@ export default function ProductForm() {
                   onChange={handleInputChange}
                 />
                 <div className="dsc-form-error">{formData.imgUrl.message}</div>
+              </div>
+              <div>
+                <FormTextArea
+                  {...formData.description}
+                  className="dsc-form-control dsc-textarea"
+                  onTurnDirty={handleTurnDirty}
+                  onChange={handleInputChange}
+                />
+                <div className="dsc-form-error">{formData.description.message}</div>
               </div>
             </div>
 
